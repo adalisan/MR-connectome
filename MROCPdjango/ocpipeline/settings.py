61038103c706273a7b33b84f6fc2ef0748bb6321
Disa Mhembere <dmhembe1@jhu.edu>
2013-02-27 16:13:03 -0500

    static file serving configuration

--- MROCPdjango/ocpipeline/settings.py
+++ MROCPdjango/ocpipeline/settings.py
@@ -80,7 +80,12 @@ print (PROCESSING_SCRIPTS)
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+
+if os.path.exists('/home/disa/MR-connectome/MROCPdjango/static'):
+  STATIC_ROOT = '/home/disa/MR-connectome/MROCPdjango/static'
+else:
+  STATIC_ROOT = '/Users/disa/MR-connectome/MROCPdjango/static'
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
