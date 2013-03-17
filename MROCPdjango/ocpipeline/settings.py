dd0f802cb7556ecb362ca5ade993beec1a25ee05
Disa Mhembere <dmhembe1@jhu.edu>
2013-03-17 17:26:41 -0400

    dynamic script prefix test

--- MROCPdjango/ocpipeline/settings.py
+++ MROCPdjango/ocpipeline/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 #    'django.core.context_processors.debug',
 #    'django.core.context_processors.i18n',
 #    'django.core.context_processors.media',
-#    'django.core.context_processors.request', # Access request data from template
+    'django.core.context_processors.request', # Access request data from template
     'django.core.context_processors.static', # {{STATIC_URL}}
     'django.contrib.auth.context_processors.auth', # For use with user An auth.User instance
 #    'django.contrib.messages.context_processors.messages',
