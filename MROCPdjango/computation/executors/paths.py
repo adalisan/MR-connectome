fba9c2383dcad897ac80f95b9627d1e568d75719
Disa Mhembere <dmhembe1@jhu.edu>
2013-06-25 13:19:04 -0400

    intermediate

--- MROCPdjango/computation/executors/paths.py
+++
@@ -1,9 +0,0 @@
-#
-# Code to load necessary paths
-#
-
-import os, sys
-
-COMPUTATION_BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "/.." ))
-sys.path += [COMPUTATION_BASE_PATH]
-
